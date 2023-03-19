0f408be952b30c3a447166ab7ec240174735bf24
ibn-A <ibn_ali@yahoo.com>
2023-03-19 16:28:02 +0100

    Mise en place de la vue listings & du modèle d'URL associé a cette vue

--- listings/views.py
+++ listings/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def hello(request):
     return HttpResponse('<h1>Hello django ! </h1>')
+
+def about(request):
+    return HttpResponse('<h1> A propos </h1> <p>Nous adorons merchex</p>')
+
+def listings(request):
+    return HttpResponse('<h1>Liste des annonces</h1>')
