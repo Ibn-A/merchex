50ace61efb5a0aad4dd628ce0ef087b07ac5e05f
ibn-A <ibn_ali@yahoo.com>
2023-03-20 07:06:36 +0100

    Affichage d'objets Listing : - Création d'objets Listing via django shell - Update de la vue listings pour afficher la liste des annonces

--- listings/views.py
+++ listings/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from listings.models import Band
+from listings.models import Listing
 
 def hello(request):
     bands = Band.objects.all()
@@ -19,7 +20,14 @@ def about(request):
     return HttpResponse('<h1> A propos </h1> <p>Nous adorons merchex</p>')
 
 def listings(request):
-    return HttpResponse('<h1>Liste des annonces</h1>')
+    listings = Listing.objects.all()
+    return HttpResponse(f"""
+        <h1>Liste des annonces</h1>
+        <ul>
+            <li>{listings[0].title}</li>
+            <li>{listings[1].title}</li>
+        </ul>
+    """)
 
 def contact(request):
     return HttpResponse('<h1>Et si nous gardions le contact !</h1>')
