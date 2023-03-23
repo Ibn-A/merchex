15528f50a1ed4031610fca5023361dcf2d00eb37
ibn-A <ibn_ali@yahoo.com>
2023-03-23 06:55:49 +0100

    Update Band model :    - Ajout divers champs & arguments    - generation & execution des migrations

--- listings/models.py
+++ listings/models.py
@@ -3,16 +3,17 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Band(models.Model):
 
-    class Genre():
+    class Genre(models.TextChoices):
         HIP_HOP = 'HH'
         SYNTH_POP = 'SP'
         ALTERNATIVE_ROCK = 'AR'
 
     name = models.fields.CharField(max_length=100)
-    genre = models.fields.CharField(choices=Genre.choices, max_length=50)
-    biography = models.fields.CharField(max_length=1000)
+    genre = models.fields.CharField(default = 'HH', choices=Genre.choices, max_length=50)
+    biography = models.fields.CharField(null=True, max_length=1000)
     year_formed = models.fields.IntegerField(
-    validators=[MinValueValidator(1900),MaxValueValidator(2023)]
+        default = 2000,
+        validators=[MinValueValidator(1900),MaxValueValidator(2023)]
     )
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
