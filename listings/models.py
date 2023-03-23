3c8bea16847e5a5e7c86d8edf4556914bd41e7d7
ibn-A <ibn_ali@yahoo.com>
2023-03-23 05:37:33 +0100

    Update modèle Band :    - Ajout de la classe imbriquée Genre.

--- listings/models.py
+++ listings/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Band(models.Model):
+
+    class Genre():
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
-    genre = models.fields.CharField(max_length=50)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=50)
     biography = models.fields.CharField(max_length=1000)
     year_formed = models.fields.IntegerField(
     validators=[MinValueValidator(1900),MaxValueValidator(2023)]
