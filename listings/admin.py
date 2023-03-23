76b133b9360339fbc42b510156fa14093dc8e724
ibn-A <ibn_ali@yahoo.com>
2023-03-23 07:46:14 +0100

    Customizaton affichage listings annonces(suite)

--- listings/admin.py
+++ listings/admin.py
@@ -6,5 +6,9 @@ from listings.models import Listing
 class BandAdmin(admin.ModelAdmin):
     list_display = ('name', 'year_formed', 'genre')
 
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'year', 'sold')
+
+
 admin.site.register(Band, BandAdmin)
-admin.site.register(Listing)
+admin.site.register(Listing, ListingAdmin)
