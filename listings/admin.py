773a1bdfe5dda875e22bb148ace2544e4b0bb950
ibn-A <ibn_ali@yahoo.com>
2023-03-23 07:40:58 +0100

    Update admin.py :    - affichage des annonces dans l'interface django

--- listings/admin.py
+++ listings/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 from listings.models import Band
+from listings.models import Listing
 
 class BandAdmin(admin.ModelAdmin):
     list_display = ('name', 'year_formed', 'genre')
+
 admin.site.register(Band, BandAdmin)
+admin.site.register(Listing)
